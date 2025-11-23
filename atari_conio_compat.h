83dc590c2c9e7ad7d7fa192e66b076430a51f8cc
Marcin Grzegorzewski <grzegorzewski@flyingdog.de>
2025-11-23 03:53:22 +0100

    duplicator 16

--- atari_conio_compat.h
+++ atari_conio_compat.h
@@ -3,29 +3,22 @@
 
   This file provides function declarations for duplicator_game.c.
   It works with both 8x8 and 16x16 modes.
+
+  - For 8x8 mode (duplicator.c): includes atari_conio.h
+  - For 16x16 mode (duplicator_16x16.c): provides declarations only
 */
 
 #ifndef ATARI_CONIO_COMPAT_H
 #define ATARI_CONIO_COMPAT_H
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-typedef unsigned char byte;
-typedef unsigned short word;
-
-/* Function declarations that duplicator_game.c needs */
-/* These are implemented in either atari_conio.c or duplicator_conio_16x16.c */
-void my_cputcxy(byte x, byte y, byte character);
-void my_clrscr(void);
-void my_cputsxy(byte x, byte y, const char* str);
-void my_cprintf_status(byte b, byte t, byte m);
-void wait_vblank(void);
-
-/* POKE/PEEK macros */
-#define POKE(addr,val)     (*(unsigned char*) (addr) = (val))
-#define PEEK(addr)         (*(unsigned char*) (addr))
+/* Check if we're in 16x16 mode by seeing if duplicator_conio_16x16.h was included */
+#ifndef DUPLICATOR_CONIO_16X16_H
+    /* 8x8 mode - just include the regular header */
+    #include "atari_conio.h"
+#else
+    /* 16x16 mode - declarations are already provided by duplicator_conio_16x16.h */
+    /* Nothing to do here */
+#endif
 
 #endif /* ATARI_CONIO_COMPAT_H */
 
