904dc6c9296215e57db3a6ef16ca8ffc2d33b742
Marcin Grzegorzewski <grzegorzewski@flyingdog.de>
2025-12-06 00:06:13 +0100

    exp files to remove

--- satanic_conio_16x16.h
+++ satanic_conio_16x16.h
@@ -29,34 +29,3 @@ byte map_tile_to_16x16(byte tile);
 
 #endif
 
-#ifndef SATANIC_CONIO_16X16_H
-#define SATANIC_CONIO_16X16_H
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "satanic_tiles_16x16.h"  // For TILE_TL, TILE_TR, TILE_BL, TILE_BR and tile definitions
-
-// Data types
-typedef unsigned char byte;
-typedef unsigned short word;
-
-// Screen dimensions for 16x16 tiles
-#define CHAR_COLS 40
-#define CHAR_ROWS 24
-#define TILE_COLS 20
-#define TILE_ROWS 12
-
-// Memory locations
-#define SCREEN_MEM  ((byte*)0x9000)
-
-// Function prototypes for 16x16 mode
-void my_clrscr_16x16(void);
-void my_cputcxy_16x16(byte tx, byte ty, byte tile_char);
-void wait_vblank_16x16(void);
-
-// Tile mapping function
-byte map_tile_to_16x16(byte tile);
-
-#endif
-
